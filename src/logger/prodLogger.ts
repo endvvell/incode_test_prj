8ee179d1725b7701dc8486d3d118e11fa8a6fdd9
Ivan <ivanarshh@gmail.com>
2022-10-03 20:09:33 +0300

    added logger

---
+++ src/logger/prodLogger.ts
@@ -0,0 +1,30 @@
+import winston from "winston";
+
+export const buildLogger = () => {
+    const logger = winston.createLogger({
+        level: 'http',
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.errors({
+                stack: true,
+            }),
+            winston.format.prettyPrint(),
+            winston.format.json(),
+        ),
+        transports: [
+            new winston.transports.File({
+                filename: './errors.log',
+                level: 'error',
+            }),
+            new winston.transports.File({
+                filename: './all-info.log',
+                level: 'info',
+            }),
+        ],
+    })
+
+    return logger
+}
+
+
+export const logger = buildLogger()
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose'
 import { createApp } from './app'
 import { MONGODB_URI } from './configs/mongo-config'
-
+import { logger } from './logger/prodLogger'
 ;(async () => {
     // connecting to dbs here:
     try {
         await mongoose.connect(MONGODB_URI).then(() => {
-            console.log('Connected to MongoDB successfully')
+            logger.info('Connected to MongoDB successfully')
         })
     } catch (error) {
-        // <log.error>
+        logger.error(`Error while connecting to MongoDB: ${error}`)
         process.exit()
     }
 
@@ -19,7 +19,7 @@ import { MONGODB_URI } from './configs/mongo-config'
     app.listen(3000, () => {
         console.log('Listening on port 3000, running in dev')
     }).on('error', (error) => {
-        // <log the error here>
+        logger.error(`Error while starting up: ${error}`)
         process.exit()
     })
 })()
